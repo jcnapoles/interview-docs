57610553cafe364b8e8ba8ff024531fd740e8b34
O2F-IT\juliocesar.napolespu <juliocesar.napolespuente@amaris.com>
2024-04-09 10:19:23 +0200

    Project prepared to add content

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,13 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Fullstack Questions</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/jcnapoles/interview-docs',
+  },  
+  docsRepositoryBase: 'https://github.com/jcnapoles/interview-docs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Fullstack Questions for Interviews',
   },
 }
 
